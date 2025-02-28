f21ac6d89c9157a29b1817c30d371e1778495722
Leonardo Negreiros <159206831+LeoNP8@users.noreply.github.com>
2025-02-28 10:43:56 -0300

    Add files via upload

---
+++ leo.py
@@ -0,0 +1,14 @@
+#Exercício 1
+nome = input("qual seu nome?") 
+nota1 = float (input("qual á sua primeira nota"))
+nota2 = float (input("qual á sua primeira nota"))
+
+print(nome,"sua nota é:")
+print((nota1 + nota2)/2)
+
+#Exercício 2
+nome = input("Digite o seu nome: ")
+idade = input("Digite a sua idade: ")
+
+print("Nome: ", nome)
+print("idade: ", idade)
